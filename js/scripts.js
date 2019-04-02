f9dbd6e96f9ac73ff1dc16e2f1e21ce2ff2a150d
Brian Hensley and Colton Lacey <brian@>
2019-04-02 15:47:28 -0700

    add js scripts to search for specefic input ids, then return the result associated with it.

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,31 @@ $(document).ready(function(){
   $("form#survey").submit(function(event){
     $("form").hide();
     event.preventDefault();
+    var alt1 = ["1"];
+    var alt2 = ["7"];
+    var alt3 = ["11"];
     var unluckyArray = []
     $("input:checkbox[name=unlucky]:checked").each(function(){
       var unluckyMode = $(this).val();
       unluckyArray.push(unluckyMode);
       for (var index = 0; index < unluckyArray.length; index += 1){
-        
+        if (alt1.includes(unluckyArray[index])) {
+          $(".result1").show();
+          $(".fortune").show();
+          return;
+        } else if (alt2.includes(unluckyArray[index])) {
+          $(".result2").show();
+          $(".fortune").show();
+          return;
+        } else if (alt3.includes(unluckyArray[index])) {
+          $(".result3").show();
+          $(".fortune").show();
+          return;
+        } else {
+          $(".result4").show();
+          $(".fortune").show();
+        }
       }
     });
-    alert(unluckyArray);
   });
 });
